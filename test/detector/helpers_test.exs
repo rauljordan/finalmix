9b3b499d9b493f8393ac1092ff32eb4f1351cf37
Raul Jordan <raul@prysmaticlabs.com>
2020-12-26 11:47:50 -0600

    fmt

--- test/detector/helpers_test.exs
+++ test/detector/helpers_test.exs
@@ -2,6 +2,8 @@ defmodule FinalMixTest.Detector.Helpers do
   use ExUnit.Case
   alias FinalMix.Detector.Helpers
   alias Ethereum.Eth.V1alpha1.IndexedAttestation
+  alias Ethereum.Eth.V1alpha1.AttestationData
+  alias Ethereum.Eth.V1alpha1.Checkpoint
   doctest FinalMix
 
   describe "group by validator index" do
@@ -14,19 +16,19 @@ defmodule FinalMixTest.Detector.Helpers do
         2 => [att2]
       }
 
-      assert Helpers.group_by_validator_index([att1, att2]) == wanted
+      assert Helpers.group_by_validator_chunk_index([att1, att2]) == wanted
     end
 
     test "properly groups multiple attestations within same subqueue" do
-      att1 = IndexedAttestation.new(attesting_indices: [0])
-      att2 = IndexedAttestation.new(attesting_indices: [1])
-      att3 = IndexedAttestation.new(attesting_indices: [2])
-
+      atts = [
+        %IndexedAttestation{attesting_indices: [0]},
+        %IndexedAttestation{attesting_indices: [1]},
+        %IndexedAttestation{attesting_indices: [2]}
+      ]
       wanted = %{
-        0 => [att3, att2, att1]
+        0 => atts
       }
-
-      assert Helpers.group_by_validator_index([att1, att2, att3]) == wanted
+      assert Helpers.group_by_validator_chunk_index(atts) == wanted
     end
 
     test "advanced properly groups multiple attestations" do
@@ -42,11 +44,32 @@ defmodule FinalMixTest.Detector.Helpers do
         1 => [att5, att6]
       }
 
-      assert Helpers.advanced_group_by_validator_index([att1, att2, att3, att4, att5, att6]) ==
+      assert Helpers.advanced_group_by_validator_chunk_index([att1, att2, att3, att4, att5, att6]) ==
                wanted
     end
   end
 
   describe "group by chunk index" do
+    test "properly groups simple, single attestations" do
+      att1 = %IndexedAttestation{
+        data: %AttestationData{
+          source: %Checkpoint{
+            epoch: 1,
+          }
+        }
+      }
+      att2 = %IndexedAttestation{
+        data: %AttestationData{
+          source: %Checkpoint{
+            epoch: 4097,
+          }
+        }
+      }
+      wanted = %{
+        0 => [att1],
+        1 => [att2]
+      }
+      assert Helpers.group_by_chunk_index([att1, att2]) == wanted
+    end
   end
 end
